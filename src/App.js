81ace76569f4ee837d4d303deee15ba6e0d95e46
SaiKiran <saikiranchatlapalli84@gmail.com>
2022-06-30 17:25:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import FileUpload from './components/fileupload/fileupload'
+import Forgotpassword from './components/forgotpassword/forgotpassword'
+import Resetpassword from './components/resetpassword/resetpassword'
+import Landingpage from './components/landingpage/landingpage'
+import Signup from './components/signup/signup'
+import Login from './components/login/login'
+import { positions, Provider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+import ProtectedRoute from './components/protected-route/protectedroute'
+import Dashboard from './components/dashboard/dashboard'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+function App() {
+
+  const options = {
+    timeout: 3000,
+    position: positions.TOP_RIGHT
+  };
+  return <> <Provider template={AlertTemplate} {...options}>
+    <BrowserRouter>
+      <Switch>
+        <ProtectedRoute path="/app" component={Dashboard} />
+        <ProtectedRoute path = "/upload" component={FileUpload}/>
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Landingpage}/>
+        <Route exact path="/forgotpassword" component={Forgotpassword} />
+        <Route path="/resetpassword/:resetToken" component={Resetpassword} />
+      </Switch>
+    </BrowserRouter>
+
+
+  </Provider>
+  </>
+}
+
+
+export default App;
---
+++ src/components/fileupload/fileupload.js
@@ -0,0 +1,140 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useHistory ,Link} from "react-router-dom"; 
+import auth from '../../service/auth'
+import './fileupload.css'
+
+import { useAlert } from "react-alert";
+
+
+const FileUpload = () => {
+  const alert = useAlert();
+  const parseJwt = (token) => {
+    try {
+      return JSON.parse(atob(token.split('.')[1]));
+    } catch (e) {
+      return null;
+    }
+  };
+  let token = localStorage.getItem('token')
+  let myemail = parseJwt(token).email
+  let myuserid = parseJwt(token).userid
+  let youremail = myemail.slice(0, -10);
+
+
+
+  const [file, setFile] = useState(null);
+
+
+  const submitFile = async (e) => {
+    e.preventDefault()
+    try {
+      if (!file) {
+        throw new Error('Select a file first!');
+      }
+      const formData = new FormData();
+      formData.append('image', file[0]);
+      const result = await axios.post(`https://sri-uploader.herokuapp.com/upload`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(result)
+      const data = {
+        email: myemail,
+        location: result.data.Location,
+        filename: file[0].name,
+        userid: myuserid
+      }
+      console.log(data)
+      const filedata = await axios.post(`https://sri-uploader.herokuapp.com/file`, data, {
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization': token,
+        },
+      })
+      console.log(filedata)
+    
+      if (filedata) {
+        alert.success(filedata.data.message);
+
+      } else {
+        alert.error('Unable to create');
+      }
+      // handle success
+    } catch (error) {
+      alert.error('Unable to createe');
+    }
+  };
+
+  let history = useHistory();
+
+  const Logout = () => {
+    auth.logoutUser();
+    history.push("/");
+  };
+
+  return (
+    <>
+ <div className="container-fluid">
+      <h1 style={{ justifyContent: "center", textAlign: "center" }}>
+        {" "}
+        UPLODER<sup>2</sup>
+      </h1>
+      <div className="h5 mb-0 font-weight-bold text-danger">
+     <Link to="/app">
+        <button className="btn btn-primary float-left" >
+          check your files
+        </button>
+        </Link> 
+      </div>
+      <div className="h5 mb-0 font-weight-bold text-danger">
+        <button className="btn btn-dark float-right" onClick={Logout}>
+          logout
+        </button>
+      </div>
+      <div>
+        <h1
+          style={{ justifyContent: "center", textAlign: "center" }}
+          id="fix"
+          className="text-primary center"
+        >
+          {youremail}
+        </h1>
+      </div>
+
+      </div>
+      <div class="container">
+        <div class="row">
+        <div className="col-md-6">
+            <h3>You can upload your stuff here</h3>
+            <p>you can drag and drop or choose the file</p>
+          </div>
+          <div class="col-md-6">
+            <form class="p-1"onSubmit={submitFile}><div class="form-group files color">
+              <label>Upload Your File </label>
+              
+              <input type="file" class="form-control" onChange={event => setFile(event.target.files)} /><br/>
+              <button type="submit" className="btn btn-primary my-2 my-sm-0 p-2 float-right">upload</button>
+            </div>
+            </form> </div>
+        </div>
+        </div>
+
+
+
+
+
+
+
+
+    
+
+
+
+    </>
+  );
+};
+
+export default FileUpload;
